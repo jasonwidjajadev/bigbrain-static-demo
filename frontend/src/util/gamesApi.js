e4c64c168f8fa0c852afd35fb0964cafaa89fa28
MarkBastoulis <mbastoulis@gmail.com>
2025-04-12 10:32:39 +1000

    Function for PUT games

--- frontend/src/util/gamesApi.js
+++ frontend/src/util/gamesApi.js
@@ -4,10 +4,21 @@ import { apiCall } from "./apiCall";
 export const fetchGames = async (adminToken) => {
   // GET implementation
   const data = await apiCall("/admin/games", "GET", null, adminToken);
+  if (data.error) {
+    // TODO: Handle error
+    console.error("Error fetching games:", data.error);
+    throw new Error(data.error);
+  }
   return data.games;
 };
 
 // Function to push all the games
-export const updateAllGames = async (allGames) => {
+export const updateAllGames = async (allGames, adminToken) => {
   // PUT implementation
+  try {
+    const response = await apiCall("/admin/games", "PUT", allGames, adminToken);
+    return response;
+  } catch (error) {
+    throw new Error(error || "Something went wrong");
+  }
 };
