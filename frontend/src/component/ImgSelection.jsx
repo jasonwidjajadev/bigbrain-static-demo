d5b13d63ad80b4ee93cd1e11f7041424db68c65a
MarkBastoulis <mbastoulis@gmail.com>
2025-04-12 10:47:53 +1000

    Added functioinality to show the name of an image once uploaded

--- frontend/src/component/ImgSelection.jsx
+++ frontend/src/component/ImgSelection.jsx
@@ -1,7 +1,18 @@
-// import React, { useState } from "react";
+import { useState } from "react";
 import { LuUpload } from "react-icons/lu";
 
 const ImgSelection = ({ handleImgChange }) => {
+  const [fileName, setFileName] = useState(null);
+
+  // Handle file selection and update filename
+  const handleFileSelect = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      setFileName(file.name);
+      handleImgChange(e);
+    }
+  };
+
   return (
     <>
       <div className="bg-white w-full border-2 border-gray-500 border-dashed drop-shadow-md/25 rounded-lg p-8 mb-6 flex flex-col items-center justify-center  transition-colors">
@@ -9,6 +20,12 @@ const ImgSelection = ({ handleImgChange }) => {
           Cover Image
         </h2>
         {/* <p className="text-gray-600 mb-4">Drag and Drop or</p> */}
+
+        {/* Show file name if available */}
+        {/* TODO: Update to show image as preview*/}
+        {fileName && (
+          <p className="mb-4 text-sm text-gray-600">File name: {fileName}</p>
+        )}
 
         <div className="flex space-x-4">
           {/* TODO: use image uploader if we have time for bonus */}
@@ -18,10 +35,10 @@ const ImgSelection = ({ handleImgChange }) => {
           </button> */}
           <label className="flex items-center px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 transition-colors cursor-pointer">
             <LuUpload className="w-5 h-5 mr-2" alt="Upload via a file icon" />
-            <span>Upload a File</span>
+            <span>{fileName ? "Change Image" : "Upload a File"}</span>
             <input
               type="file"
-              onChange={handleImgChange}
+              onChange={handleFileSelect}
               className="hidden"
               accept="image/*"
             />
