e33a800577267365952dc1002abb889fa4df7a8a
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-14 00:48:15 +1000

    moved folder

---
+++ frontend/src/pages/auth/AuthLogout.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+import { useAuthContext } from "../../context/useAuthContext";
+import { apiCall } from '../../util/apiCall';
+
+function AuthLogout() {
+  const {token, logout } = useAuthContext();
+  const hasLoggedOut = useRef(false);
+
+  useEffect(() => {
+    if (!token  || hasLoggedOut.current ) return;
+    const doLogout = async () => {
+      try {
+        await apiCall('/admin/auth/logout', 'POST', null, token);
+      } catch (err) {
+        console.error('Logout failed:', err.message);
+      } finally {
+        hasLoggedOut.current = true;
+        logout();
+      }
+    };
+    doLogout();
+  }, [token, logout]);
+
+  useEffect(() => {
+    if (token === null) {
+      console.log("✅ token has been cleared");
+    }
+  }, [token]);
+  return null;
+}
+
+export default AuthLogout;
