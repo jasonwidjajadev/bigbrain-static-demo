f457a370cc6b399c303e7206e1a9c474c5b3edb1
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-14 01:03:56 +1000

    added hamburger icon for small screen

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,6 +6,10 @@ import { RiAddCircleLine } from "react-icons/ri";
 import { orangeButtonClass } from "../component/tailwind";
 import { fetchGames } from "../util/gamesApi";
 import GameDashboardTile from "../component/GameDashboardTile";
+import JoinGameButton from '../component/JoinGameButton';
+import { VscThreeBars } from "react-icons/vsc";
+import { TbLogout } from "react-icons/tb";
+import { FaPlay } from "react-icons/fa";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -46,31 +50,59 @@ function Dashboard() {
 
   return (
     <div className="min-h-screen overflow-y-auto flex flex-col">
-      {/* Navbar */}
+      {/* Navbar Left Side*/}
       <nav className="flex justify-between items-center px-4 sm:px-8 py-2.5 bg-cyan-200 h-[65px]">
-        <Link
-          to="/home"
-          className="text-orange-500 text-3xl font-bold no-underline"
-        >
-          <LogoNavBar />
-        </Link>
-        <div className="flex gap-3 items-center">
-          <Link
-            to="/quiz/create"
-            className={`${orangeButtonClass} flex items-center gap-2`}
-          >
-            <RiAddCircleLine className="text-2xl" /> Create
-          </Link>
-          <Link to="/auth/logout" className={`${orangeButtonClass} px-5`}>
-            Log out
-          </Link>
+        <Link to="/home" className="text-orange-500 text-3xl font-bold no-underline"><LogoNavBar /></Link>
+
+        {/* //* NavBar Right side For Small Screen Dropdown */}
+        <div className="dropdown dropdown-bottom dropdown-end sm:hidden">
+          {/* Toggle Button */}
+          <div tabIndex={0} role="button" className={orangeButtonClass}>
+            <VscThreeBars className="text-3xl" />
+          </div>
+
+          {/* Dropdown Content */}
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow-lg bg-orange-500 rounded-box w-52 z-50 text-xl text-white">
+            <li>
+              <Link to="/quiz/create" className="flex items-center gap-[16px]">
+                <RiAddCircleLine /> Create
+              </Link>
+            </li>
+            <li>
+              <Link to="/quiz/join" className="font-semibold flex items-center gap-[20px]">
+                <FaPlay className="text-[16px]"/> Play
+              </Link>
+            </li>
+            <li>
+              <Link to="/auth/logout" className="flex items-center gap-3">
+                <TbLogout className="text-2xl"/> Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* //*NavBar Right side For Big Screen Dropdown */}
+        <div className="hidden sm:block">
+          <div className="flex gap-3 items-center">
+            {/* Create */}
+            <Link to="/quiz/create" className={`${orangeButtonClass} flex items-center gap-2`}>
+              <RiAddCircleLine className="text-2xl" /> Create
+            </Link>
+            {/* Play */}
+            <JoinGameButton />
+            {/* Logout */}
+            <Link to="/auth/logout" className={`${orangeButtonClass} px-5 flex items-center gap-2`}> 
+              <TbLogout className="text-2xl"/> Logout
+            </Link>
+          </div>
         </div>
       </nav>
 
-      {/* Games Feed */}
+
+      {/* //*Games Feed */}
       <div className="flex-1 flex flex-col justify-start items-center text-center p-8">
         <div className="w-full max-w-6xl">
-          <h1 className="text-5xl font-semibold text-left mb-6 text-orange-500 font-Nunito-Black">
+          <h1 className="text-5xl font-semibold text-left mb-6 text-orange-500 font-Nunito-ExtraBold">
             My Games
           </h1>
 
