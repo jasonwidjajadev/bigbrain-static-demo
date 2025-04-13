f0f0a42cd39c6cb7194372eb8a0931fb9f35a5eb
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-14 00:53:55 +1000

    made this compononent more modular by adding props

--- frontend/src/component/LogoBigRotate.jsx
+++ frontend/src/component/LogoBigRotate.jsx
@@ -1,9 +1,9 @@
 import logonoborder from '../assets/logonoborder.png';
 
-const LogoBigRotate = () => {
+export const LogoBigRotate = ({ sizeClass = "h-[70px] sm:h-[95px]", addClassName = ""}) => {
   return (
     <img src={logonoborder}
-      className="h-[75px] sm:h-[95px] shrink-0 transition-transform duration-500 ease-in-out hover:rotate-180" alt="brain-logo"
+      className={`${sizeClass} shrink-0 transition-transform duration-500 ease-in-out hover:rotate-180 ${addClassName}`} alt="brain-logo"
     />
   );
 };
