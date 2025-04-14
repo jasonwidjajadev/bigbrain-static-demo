579184760e42b43ab5942518e37095374c47b9c8
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-14 20:50:22 +1000

    added merge request

--- frontend/src/pages/QuizJoin.jsx
+++ frontend/src/pages/QuizJoin.jsx
@@ -1,114 +1,101 @@
 /**
-https://www.bigbrain/com
-https://dashboard.bigbrain.com
-https://play.bigrain.com/play
+                quizzes:
+                  "994004996": {
+                  "name": "monkeyfriday",
+                  "owner": "kaws@gmail.com",
+                  "questions": [],
+                  "thumbnail": null,
+                  "active": null,
+                  "createdAt": "2025-04-13T16:29:17.563Z"
+                }
+              session:
+                  gamepin generated "982370": {
+              "quizId": "994004996",
+              "position": -1,
+              "isoTimeLastQuestionStarted": null,
+              "players": {},
+              "questions": [],
+              "active": true,
+              "answerAvailable": false
+            },
+HOST
+dashboard,
+    0.
+    1.host click play -> this automatically start quiz (ui card has automatically stop quiz and view quiz)
+    2 generate ID, Join here 431288, OR copy link with the url
+      -> copylink and (backend generates new number each time)
+      -> open quiz  -> quiz/play/431288, music, animation, text moving
+                    -> option to start quiz
+                    -> option to end quiz
+                    -> option to go back and have multiple quiz happenign at the same time
+              -> host can got back to dashboard where there is a stop
+              -> or view quiz back to previous state
+      -> start or END (end shows leaderboard on host side, on player side shows incorrect answer should have a default host ended game early)
 
-host click play
-host/join
-  -> dashboardpage -> 1.host click >Host
-  -> https://classic.blooket.com/host/join , generate id
-        -> 7287587
-        -> copy link with the url
-
-user1
-  -> https://play.blooket.com/play (navbar: homepage + login)
-            -> error message invalid game id or no game id
-                - Invalid game ID
-                - Please enter a game ID
-
-  -> https://classic.blooket.com/play/register: Enter a Nickname
-            -> error message "Please Provide a Nickname"
-  -> automatically host, populates: https://classic.blooket.com/host/join
+  THE LOBBY
+  -> automatically host, populates of users who joined: https://classic.blooket.com/host/join
   -> https://classic.blooket.com/play/lobby
             -> chosen avatar by default, if user change avatar it gets updates in host page
             -> shows a message waiting for host, (can play game if want to)
             -> if user disconnect, host page removes the user
   -> host start hgame
 
-
-HOST
-dashboard, 
-    1.host click play -> this automatically start quiz (ui card has automatically stop quiz and view quiz)
-  -> generate ID, Join here 431288,
-      -> copylink and (backend generates new number each time)
-          -> quiz/play/431288, music, animation, text moving
-              -> host can got back to dashboard where there is a stop
-              -> or view quiz back to previous state
-      -> start or END (end shows leaderboard on host side, on player side shows incorrect answer should have a default host ended game early)
-
-      quizzes:
-          "994004996": {
-          "name": "monkeyfriday",
-          "owner": "kaws@gmail.com",
-          "questions": [],
-          "thumbnail": null,
-          "active": null,
-          "createdAt": "2025-04-13T16:29:17.563Z"
-        }
-      session:
-          gamepin generated "982370": {
-      "quizId": "994004996",
-      "position": -1,
-      "isoTimeLastQuestionStarted": null,
-      "players": {},
-      "questions": [],
-      "active": true,
-      "answerAvailable": false
-    },
 PLAYER
   -> 1.quiz/join, enter invalid quizID regardless brings into part 2 -> where it fails if invalid quizId
   -> 2.enter valid pin, quiz/join/sessionid bring into please enter name
-      -> msut enter a name
+      -> msut enter a name,
               -> this is where the error is invalid quiz ID, popup on the bottom
-              -> quizId is already finish
+                    -> error message invalid game id or no game id
+                        - Invalid game ID
+                        - Please enter a game ID
+              -> OR quizId is not active
   -> 3. enters into lobby, quiz/play/431288 waiting for HOST
 
  */
 
+// TODO can an admin play its own game? i.e should disable clicking
+// TODO: Should have an error if invalid game ID
 
-
-/**
- import { useLocation } from 'react-router-dom';
-
-function PlayerGameEnterName() {
-  const location = useLocation();
-  const nickname = location.state?.nickname || localStorage.getItem('nickname');
-
-  if (!nickname) {
-    // Redirect back or show error
-    navigate('/quiz/join');
-  }
-
-  // ...
-}
-
-
- */
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import config from '../../backend.config.json';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import LogoNavBar from '../component/LogoNavBar';
 import { orangeButtonClass } from '../component/tailwind';
 import { useAuthContext } from '../context/useAuthContext';
-// import keyboard_big_brain from '../assets/keyboard_big_brain.png';
 import LogoBigRotate from '../component/LogoBigRotate';
-// import { DotPattern } from "../component/DotPattern"
+import { apiCall } from '../util/apiCall';
+import Typewriter from 'typewriter-effect';
 
 function QuizJoin() {
+  //Show on navbar wether its dashboard or login
   const { token } = useAuthContext();
   const authLink = token ? { path: '/dashboard', label: 'Dashboard' } : { path: '/auth/login', label: 'Log in' };
-  const [gameId, setGameId] = React.useState('');
+
+  //Game Id and Nickname
+  const [sessionId, setSessionId] = React.useState('');
+  const [nickName, setNickName] = React.useState('');
   const [errorMessage, setErrorMessage] = React.useState('');
   const [loading, setLoading] = React.useState(false);
 
-  // TODO can an admin play its own game? to double check this
-  // TODO: Should have an error if invalid game ID
-  // TODO: mark have 8 digit for pin
+  // Api Call
+  const navigate = useNavigate();
   async function joinGame() {
     setLoading(true);
     try {
-      console.log('Joining game with ID:', gameId);
+      console.log('Attempting to join game with ID:', sessionId);
+      console.log('Attempting to join game with nickName:', nickName);
+      const data = await apiCall(`/play/join/${sessionId}`, 'POST', {
+        name: nickName.trim(),
+      })
+
+      //Success player goes to lobby -> PlayerGameLobby
+      localStorage.setItem('playerId', data.playerId);
+      navigate(`/quiz/lobby/${sessionId}`, {
+        state: {
+          nickname: nickName,
+          isAdmin: false,
+        }
+      });
     } catch (err) {
       setErrorMessage(err.message || 'Network error. Please try again.');
     } finally {
@@ -140,7 +127,18 @@ function QuizJoin() {
       >
         <div className="flex items-center mb-6 gap-5" >
           <LogoBigRotate sizeClass="h-[50px] sm:h-[70px]"/>
-          <h1 className="text-4xl sm:text-5xl text-orange-500 whitespace-nowrap font-Nunito-ExtraBold">Big Brain</h1>
+          <div className="text-4xl sm:text-5xl text-orange-500 whitespace-nowrap font-Nunito-ExtraBold">
+            <Typewriter
+              options={{
+                strings: ['Big Brain', 'Big Fun', 'Think Fast', 'Game On!'],
+                autoStart: true,
+                loop: true,
+                pauseFor: 5000,
+                delay: 100,
+                deleteSpeed: 75,
+              }}
+            />
+          </div>
         </div>
         <div className='px-4 pt-4 pb-5 rounded-xl bg-white'>
           <div className="mb-3">
@@ -148,15 +146,17 @@ function QuizJoin() {
               type="text"
               inputMode="numeric"
               pattern="[0-9]*"
-              id="gameId"
-              name="gameId"
+              title="Please enter numbers only"
+              id="sessionId"
+              name="sessionId"
               aria-label="Game ID"
-              placeholder="Enter Game PIN"
+              placeholder="Enter Game PIN Number"
               autoComplete="off"
               required
-              value={gameId}
+              value={sessionId}
               onChange={(e) => {
-                setGameId(e.target.value);
+                const onlyNums = e.target.value.replace(/\D/g, '');
+                setSessionId(onlyNums);
                 setErrorMessage('');
               }}
 
@@ -165,19 +165,39 @@ function QuizJoin() {
           </div>
           {errorMessage && (<div className="text-red-500 text-sm mb-4" role="alert"> {errorMessage} </div>)}
 
+          <div className="mb-3">
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              title="Please enter numbers only"
+              id="sessionId"
+              name="sessionId"
+              aria-label="Game ID"
+              placeholder="Enter Nickname"
+              autoComplete="off"
+              required
+              value={sessionId}
+              onChange={(e) => {
+                // const onlyNums = e.target.value.replace(/\D/g, '');
+                setSessionId(e.target.value);
+                setErrorMessage('');
+              }}
+
+              className="placeholder:font-Nunito-Bold placeholder:text-gray-400 p-3 text-lg border border-gray-300 rounded-md w-[290px] text-center focus:outline-none focus:ring-2 focus:ring-orange-400"
+            />
+          </div>
+
           <button
             type="submit"
-            disabled={loading || !gameId}
+            disabled={loading || !sessionId}
             aria-label="Join game now"
             className={`sm:text-xl w-full px-8 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out shadow-[0_4px_0_0_#c2410c]
-              ${ loading || !gameId ? 'bg-orange-300 cursor-not-allowed text-white' : 'bg-orange-500 text-white hover:bg-orange-400 hover:-translate-y-1'}`}
+              ${ loading || !sessionId ? 'bg-orange-300 cursor-not-allowed text-white' : 'bg-orange-500 text-white hover:bg-orange-400 hover:-translate-y-1'}`}
           >
             {loading ? 'Joining...' : 'Join'}
           </button>
-
         </div>
-
-
       </form>
 
       {/* Footer */}
