5922513ad8f1a9567afac212c2a70def653f9eb0
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-15 00:34:07 +1000

    added font nunito

--- frontend/src/component/tailwind.js
+++ frontend/src/component/tailwind.js
@@ -4,4 +4,4 @@ export const orangeButtonClass = `
   hover:bg-orange-400 hover:-translate-y-1
 `;
 
-export const input = `w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-400 placeholder:text-sm`;
+export const input = `w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-400 placeholder:text-sm font-Nunito-Medium`;
