f8516e20476c535b48d5246ab0aae122343981f1
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 03:58:04 +1000

    remove comment

--- frontend/src/pages/auth/AuthLogout.jsx
+++ frontend/src/pages/auth/AuthLogout.jsx
@@ -6,7 +6,6 @@ function AuthLogout() {
   const {token, logout } = useAuthContext();
   const hasLoggedOut = useRef(false);
 
-  //TODO need to end all existing quiz
   useEffect(() => {
     if (!token  || hasLoggedOut.current ) return;
     const doLogout = async () => {
