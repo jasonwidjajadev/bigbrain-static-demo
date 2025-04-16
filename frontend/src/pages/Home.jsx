b71a0d59403bcfdc972c80c8c08b8e83a029fe0d
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 03:53:26 +1000

    updated for small screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,6 @@ function Home() {
 
   return (
     <div className="min-h-screen overflow-y-auto flex flex-col">
-
       {/* Navbar */}
       <nav className="flex justify-between items-center px-4 sm:px-8 py-2.5 text-center  bg-cyan-200 h-[65px]">
         <LinkLogoNavBar targetPath="/home" />
@@ -27,7 +26,7 @@ function Home() {
       </nav>
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col sm:flex-row items-center justify-center w-full max-w-7xl mx-auto px-4 sm:px-8 lg:px-12 pt-8 pb-30">
+      <div className="flex-1 flex flex-col sm:flex-row items-center justify-center w-full max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 pt-8 pb-30 gap-0 sm:gap-6">
         {/* //* Left Side Animation */}
         <div className="w-full sm:w-1/2 flex justify-center items-center">
           <video src={classroom} autoPlay loop muted playsInline className="w-[300px] md:w-[550px]"/>
@@ -37,7 +36,6 @@ function Home() {
         <div className="w-full sm:w-1/2 flex flex-col items-center text-center">
           <div className="flex items-center mb-6 gap-4 sm:gap-7" >
             <LogoBigRotate />
-            {/* <h1 className="text-5xl sm:text-7xl text-orange-500 whitespace-nowrap font-Nunito-ExtraBold">Big Brain</h1> */}
             <div className="text-5xl sm:text-7xl text-orange-500 whitespace-nowrap font-Nunito-ExtraBold">
               <Typewriter
                 options={{
@@ -51,18 +49,21 @@ function Home() {
               />
             </div>
           </div>
-          <div className="flex gap-6 items-center text-center">
+          <div className="flex flex-col sm:flex-row gap-6 items-center text-center w-full md:w-auto">
             <Link
               to="/auth/login"
-              className="sm:text-xl sm:px-7 px-5 py-2 rounded-md bg-orange-500 text-white no-underline font-semibold shadow-[0_4px_0_0_#c2410c]
-              transition-all duration-300 ease-in-out  hover:bg-orange-400 hover:-translate-y-1"
-            > Log in
+              className="w-full md:w-auto sm:text-xl sm:px-7 px-5 py-2.5 rounded-md bg-orange-500 text-white no-underline font-semibold shadow-[0_4px_0_0_#c2410c]
+              transition-all duration-300 ease-in-out hover:bg-orange-400 hover:-translate-y-1"
+            >
+              Log in
             </Link>
+
             <Link
               to="/auth/register"
-              className="sm:text-xl sm:px-5 border-2 border-orange-500 px-4 py-2 rounded-md bg-white text-orange-500 font-semibold no-underline
+              className="w-full md:w-auto sm:text-xl sm:px-5 border-2 border-orange-500 px-4 py-2.5 rounded-md bg-white text-orange-500 font-semibold no-underline
               transition-all duration-300 ease-in-out hover:bg-orange-50 hover:-translate-y-1 hover:shadow-[0_4px_0_0_#f97316]"
-            > Sign up
+            >
+              Sign up
             </Link>
           </div>
         </div>
