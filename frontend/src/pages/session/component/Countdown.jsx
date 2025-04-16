1dd555ed69de3e7d421b0d781aba1f5b2fab7b46
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 05:51:04 +1000

    added placholder countdown

---
+++ frontend/src/pages/session/component/Countdown.jsx
@@ -0,0 +1,14 @@
+function Countdown() {
+  return (
+    <>
+    3 2 1 ...
+
+    The question type (multiple choice, single choice, judgement)
+
+    Single choice questions have multiple answers the player can guess, ONLY one is correct
+    Multiple choice questions have multiple answers the player can guess, MULTIPLE are correct and they must select ALL correct ones
+    Judgement questions have a SINGLE answer the player can guess, the answer is either correct or incorrect
+    </>
+  )
+}
+export default Countdown;
