14cffca0ca55fb7d674c30bc0de84549538e6296
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 04:26:32 +1000

    streamline routes for app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,88 +3,195 @@ POST    /admin/auth/login                         /login
 POST    /admin/auth/register                      /register
 POST    /admin/auth/logout
 
+================================================================================
 GET     /admin/games                              /quiz/create
-PUT     /admin/games                              /quiz/update
+        Required: Token
+        Response:
+        {
+          "games": [
+            {
+              "id": 56513315,
+              "owner": "hayden@unsw.edu.au",
+              "active": null,
+              "questions": [
+                {
+                  "duration": 10,
+                  "correctAnswers": [
+                    "Answer 3",
+                    "Answer 4"
+                  ],
+                  "Answers": [
+                    {
+                      "Answer": "THIS IS JUST AN EXAMPLE"
+                    },
+                    {
+                      "Answer": "YOU CAN HAVE WHATEVER MAKES SENSE FOR YOUR QUESTION AS YOU PUT THEM BACK"
+                    },
+                    {
+                      "Answer": "Answer 3"
+                    },
+                    {
+                      "Answer": "Answer 4"
+                    }
+                  ]
+                }
+              ],
+              "AAAA": "WHATEVER YOU PUT IN A GAME",
+              "BBBB": "STORE AS MANY AS YOU WANT"
+            }
+          ]
+        }
+
+PUT     /admin/games - update multiple games owned by the admin
+        Required: Token
+        Request Body:
+        {
+          "games": [
+            {
+              "id": 56513315,
+              "owner": "hayden@unsw.edu.au",
+              "questions": [
+                {
+                  "duration": 10,
+                  "correctAnswers": [
+                    "Answer 3",
+                    "Answer 4"
+                  ],
+                  "NOTE": "correctAnswers is MANDATORY AND MUST BE IN THE QUESTION SCHEMA",
+                  "REST": "WHATEVER YOU NEED IN A QUESTION"
+                }
+              ],
+              "AAAA": "WHATEVER YOU PUT IN A GAME",
+              "BBBB": "STORE AS MANY AS YOU WANT"
+            }
+          ]
+        }
+
+//*2.3.2. Stopping a game session
+//* You can advance either in the middle of a question's duration counting down or once the question has time up.
 POST    /admin/game/:gameid/mutate - mutate a game's state (start/advance/end)
+        Required: Token
+        Required: quiz SessionId
+        {
+          "mutationType": "START | "ADVANCE" | "END"
+        }
+
+================================================================================
+//* 2.3.3. Advancing & getting the results of a game
+//* A unique route must exist for this screen that is parameterised on the session ID. E.G. /session/{session_id}
+// /session/{session_id}/
+
+//* duration counting down or once the question has time up.
+//* Once the game session has finished, it should display the following:
+
+//* A table of up to top 5 users and their score
+
+//* A bar/line chart showing a breakdown of what percentage of people (Y axis) got certain questions (X axis) correct
+//* A chart showing the average response/answer time for each question
+//* Any other interesting information you see fit (Bonus mark can be granted for this based on your implementation)
+
 GET     /admin/session/:sessionid/status - get the current status for a game session
+        Required: Token
+        Required: quiz SessionId
+        Response:
+        {
+          "results": {
+            "active": false,
+            "answerAvailable": false,
+            "isoTimeLastQuestionStarted": "2020-10-31T14:45:21.077Z",
+            "position": 2,
+            "questions": [
+              {}
+            ],
+            "numQuestions": 1,
+            "players": [
+              "Hayden"
+            ]
+          }
+        }
+
 GET     /admin/session/:sessionid/results - gets the results for game session and what people's scores were
+        Required: Token
+        Required: quiz SessionId
+        Response:
+        [
+          {
+            "name": "Hayden Smith",
+            "answers": [
+              {
+                "answerIds": [
+                  56513315
+                ],
+                "correct": false,
+                "answeredAt": "2020-10-31T14:45:21.077Z",
+                "questionStartedAt": "2020-10-31T14:45:21.077Z"
+              }
+            ]
+          }
+        ]
 
+//* PLAYER =====================================================================
 POST    /play/join/:sessionid - join an active session as a new player
-GET     /play/:playerid/status - for the current session, the player can determine if its started or not
-GET     /play/:playerid/question - for the current question that session is up to, this gets the details of the question
-GET     /play/:playerid/answer - once the question timer is finished, returns the correct answer
-PUT     /play/:playerid/answer - for the current session is up to, allows the player to submit their answer
-GET     /play/:playerid/results - once a session has ended this allows players to collec the results of their performance nenchmarked against others
+        Required session Id
+        Name: {
+          "name": "Hayden Smith"
+        }
+        Response:
+        {
+          "playerId": 2389498
+        }
+
+
+GET     /play/:playerid/status
+        for the current session, the player can determine if its started or not
+        Required: playerId
+        Response:
+        {
+          "started": true
+        }
+
+GET     /play/:playerid/question
+        for the current question that session is up to, this gets the details of the question
+        Required: playerId
+        {
+          "isoTimeLastQuestionStarted": "2020-10-31T14:45:21.077Z"
+        }
+
+GET     /play/:playerid/answer
+        once the question timer is finished, returns the correct answer
+        Required: playerId
+        Response:
+        {
+          "answerIds": [
+            0
+          ]
+        }
+
+PUT     /play/:playerid/answer
+        for the current session is up to, allows the player to submit their answer
+        Required: playerId
+        body: {
+          "answerIds": [
+            0
+          ]
+        }
+        Response: {}
+
+//* 2.5.1. Game Session Results
+//* After the final question is answered, a screen is displayed to players showing the key results:
+//* The player's performance in each question, including how many points they scored, and how many seconds they took to answer each of them.
+GET     /play/:playerid/results
+        Once a session has ended this allows players to collect the results of their performance nenchmarked against others
+        Required: playerId
+        Response:
+        [
+          {
+            "answerIds": [
+              56513315
+            ],
+            "correct": false,
+            "answeredAt": "2020-10-31T14:45:21.077Z",
+            "questionStartedAt": "2020-10-31T14:45:21.077Z"
+          }
+        ]
 */
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthProvider from './context/AuthContext';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import AuthLogin from './pages/auth/AuthLogin';
-import AuthRegister from './pages/auth/AuthRegister';
-import AuthLogout from './pages/auth/AuthLogout';
-import Dashboard from './pages/Dashboard';
-
-import QuizCreate from './pages/QuizCreate';
-import QuizEdit from './pages/QuizEdit';
-
-import QuizJoin from './pages/QuizJoin';
-import QuizJoinViaURL from './pages/QuizJoinViaURL';
-
-import HostGameLobby from './pages/host/HostGameLobby';
-import HostGamePlay from './pages/host/HostGamePlay';
-import HostGameResults from './pages/host/HostGameResults';
-
-import PlayerGameLobby from './pages/player/PlayerGameLobby';
-import PlayerGamePlay from './pages/player/PlayerGamePlay';
-import PlayerGameResults from './pages/player/PlayerGameResults';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          {/* 1. Home + Auth */}
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/auth/login" element={<AuthLogin />} />
-          <Route path="/auth/register" element={<AuthRegister />} />
-          <Route path="/auth/logout" element={<AuthLogout />} />
-
-          {/* 2. Dashboard + Quizzes */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/quiz/create" element={<QuizCreate />} />
-          <Route path='/quiz/edit/:quizId' element={ <QuizEdit/>} />
-          {/* <Route path='/quiz/edit/:quizId/:questionId' element={ <QuizEditQuestion/>} /> */}
-
-          {/* 3.1 Host + Player*/}
-          <Route path='/quiz/join' element={ <QuizJoin/>} />
-          {/* 3.2 Player choose name*/}
-          <Route path='/quiz/join/:sessionId' element={ <QuizJoinViaURL/>} />
-
-          {/* 4. Host Game Session */}
-          <Route path='/host/lobby/:sessionId' element={ <HostGameLobby/>} />
-          <Route path='/host/play/:sessionId' element={ <HostGamePlay/>} />
-          <Route path='/host/results/:sessionId' element={ <HostGameResults/>} />
-
-          {/* 5. Player Game Session */}
-          <Route path='/quiz/lobby/:sessionId' element={ <PlayerGameLobby/>} />
-          <Route path='/quiz/play/:sessionId' element={ <PlayerGamePlay/>} />
-          <Route path='/quiz/results/:sessionId' element={ <PlayerGameResults/>} />
-
-          {/* <Route path='/quiz/join/:sessionId' element={ <QuizPlay/>} /> */}
-          {/* <Route path='/lobby' element={ <Lobby/>} /> */}
-          {/* <Route path='/quiz/play/:sessionid' element={ <ActiveQuiz/>} /> */}
-          {/* <Route path='/results' element={ <AdminResults/>} /> */}
-          {/* <Route path='/game/results/:sessionid' element={ <GameResults/>} /> */}
-
-          {/* Not Found */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-  );
-}
-
-export default App
