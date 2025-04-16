ed97f50a5d4d9bf59c88bf4b50359bfb03a18c77
MarkBastoulis <mbastoulis@gmail.com>
2025-04-16 22:05:25 +1000

    Added a green button

--- frontend/src/component/tailwind.js
+++ frontend/src/component/tailwind.js
@@ -4,6 +4,12 @@ export const orangeButtonClass = `
   hover:bg-orange-400 hover:-translate-y-1
 `;
 
+export const greenButtonClass = `
+  -mt-1 sm:text-xl px-4 py-2 rounded-md bg-green-500 text-white font-semibold no-underline
+  shadow-[0_4px_0_0_#166534] transition-all duration-300 ease-in-out
+  hover:bg-green-400 hover:-translate-y-1
+`;
+
 export const input = `w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-400`;
 
 export const cyanButtonWFullClass = `
