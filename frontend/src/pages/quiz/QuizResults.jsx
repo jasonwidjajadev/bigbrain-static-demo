6f6e89a4b081b4cb5c44b3e53107b89690801107
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 04:22:05 +1000

    added placeholder

--- frontend/src/pages/quiz/QuizResults.jsx
+++ frontend/src/pages/quiz/QuizResults.jsx
@@ -2,6 +2,9 @@ function QuizResults() {
   return (
     <>
     QuizResults
+    2.5.2. 🙉🙉🙉 (For pairs only) Past game session results
+
+    Allow admins to access a page whereby they can see a list of previous sessions for a game, and then view results for those previous sessions as well.
     </>
   )
 }
