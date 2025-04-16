3747c5c57a358ecf7b3f11a089e8f7a6139443db
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 06:52:04 +1000

    added functionality for host game

---
+++ frontend/src/pages/session/component/HostGameLobby.jsx
@@ -0,0 +1,93 @@
+import { FaPlay } from "react-icons/fa";
+import { orangeButtonClass, lobbyNameClass } from '../../../component/tailwind';
+import classroom from '../../../assets/classroom_overlay.png';
+import { FaStop } from "react-icons/fa6";
+import { IoPerson } from "react-icons/io5";
+import LinkLogoNavBar from '../../../component/LinkLogoNavBar';
+import { Link } from 'react-router-dom';
+function HostGameLobby({sessionId, players, showResults, advancePosition}) {
+  return (
+    <div className="min-h-screen overflow-y-auto flex flex-col">
+
+      {/* Navbar */}
+      <nav className="flex justify-between items-center px-4 sm:px-8 py-2.5 bg-cyan-200 h-[65px] text-center">
+        <LinkLogoNavBar targetPath="/home" />
+
+        {/* For Small Screen */}
+        <div className="flex gap-3 sm:gap-4 items-center">
+          <div className='block sm:hidden pl-2'>
+            <div className='flex items-center gap-4 text-cyan-800'>
+              <div className='flex gap-2 mt-1'>
+                <IoPerson className='text-3xl'/>
+                <span className='text-3xl sm:text-4xl font-Nunito-ExtraBold'>
+                  {players.length}
+                </span>
+              </div>
+              <button
+                className={`${orangeButtonClass} flex items-center gap-2 px-5`}
+                onClick={advancePosition}
+              >
+                <FaPlay className="text-[16px]"/>Start
+              </button>
+            </div>
+          </div>
+
+          {/* End Game while in -1 position */}
+          <button
+            className={`${orangeButtonClass} flex items-center gap-3 px-5`}
+            onClick={showResults}
+          >
+            <FaStop className="text-[22px]"/>End
+          </button>
+        </div>
+      </nav>
+
+      <main
+        className="flex-1 flex flex-col items-center text-center p-6 bg-cover bg-center h-screen"
+        style={{ backgroundImage: `url(${classroom})` }}
+      >
+
+        {/* //*Quiz ID + Start Game */}
+        <div className='mb-8 flex gap-5 mx-auto justify-between items-center'>
+          <div className='py-4 px-6 rounded-md bg-cyan-800 text-white'>
+            <p className="text-md sm:text-xl font-Nunito-Medium mb-3"><span className='text-xl'>Join at&nbsp;&nbsp;</span>
+              <Link to={`/quiz/join/${sessionId}`} className='font-bold underline'>www.bigbrain.com/join</Link>
+            </p>
+            <p className="text-5xl sm:text-6xl font-Nunito-Black ">{sessionId}</p>
+          </div>
+          <div className='hidden sm:block'>
+            <div className='flex flex-col gap-5 items-center'>
+              <div className='flex items-center gap-3 text-cyan-800'>
+                <IoPerson className='text-4xl'/>
+                <span className='text-3xl sm:text-4xl font-Nunito-ExtraBold'>
+                  {players.length}
+                </span>
+              </div>
+              <button
+                onClick={advancePosition}
+                className={`${orangeButtonClass} flex items-center gap-2 px-5`}>
+                <FaPlay className="text-[16px]"/>Start
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className='flex flex-wrap gap-3 sm:gap-4 max-w-7xl mx-auto justify-center'>
+            {!players?.length && (
+              <div className={lobbyNameClass}>Waiting for players...</div>
+            )}
+            {players?.map((name, index) => (
+              <div key={index} className={lobbyNameClass}>
+                {name}
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+export default HostGameLobby;
