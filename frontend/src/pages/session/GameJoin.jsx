1c2ae3f58f67eecd1124acbaad6debc4595633bd
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-17 04:56:33 +1000

    added logic for game Join

---
+++ frontend/src/pages/session/GameJoin.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import LinkLogoNavBar from '../../component/LinkLogoNavBar';
+import { orangeButtonClass } from '../../component/tailwind';
+import { useAuthContext } from '../../context/useAuthContext';
+import LogoBigRotate from '../../component/LogoBigRotate';
+import { apiCall } from '../../util/apiCall';
+import Typewriter from 'typewriter-effect';
+
+function GameJoin() {
+
+  //Show on navbar wether its dashboard or login
+  const { token } = useAuthContext();
+  const authLink = token ? { path: '/dashboard', label: 'Dashboard' } : { path: '/auth/login', label: 'Log in' };
+
+  //Game Id and Nickname
+  const [sessionIdInput, setSessionIdInput] = React.useState('');
+  const [nickName, setNickName] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [isNickFocused, setIsNickFocused] = React.useState(false);
+
+  // Api Call
+  const navigate = useNavigate();
+  async function goToLobby() {
+    setLoading(true);
+    try {
+      console.log('Attempting to join game with ID:', sessionIdInput);
+      console.log('Attempting to join game with nickName:', nickName);
+      const data = await apiCall(`/play/join/${sessionIdInput}`, 'POST', {
+        name: nickName.trim(),
+      })
+
+      //Success player goes to lobby -> PlayerGameLobby
+      localStorage.setItem('playerId', data.playerId);
+      navigate(`/quiz/lobby/${sessionIdInput}`, {
+        state: {
+          nickname: nickName,
+          isAdmin: false,
+        }
+      });
+
+    } catch (err) {
+      setErrorMessage(err.message || 'Network error. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrorMessage('');
+    goToLobby();
+  };
+
+  return (
+    <div className="min-h-screen overflow-y-auto flex flex-col">
+      {/* Navbar */}
+      <nav className="flex justify-between items-center px-4 sm:px-8 py-2.5 bg-cyan-200 h-[65px] text-center">
+        <LinkLogoNavBar targetPath="/home" />
+        <Link to={authLink.path} className={`${orangeButtonClass} px-6`}>{authLink.label}</Link>
+      </nav>
+
+      {/* Join Game Form */}
+      <form
+        onSubmit={handleSubmit}
+        aria-label="Join Game form"
+        className="flex-1 flex flex-col justify-center items-center text-center p-8 bg-cyan-700 pb-25"
+      >
+        <div className="flex items-center mb-6 gap-5" >
+          <LogoBigRotate sizeClass="h-[50px] sm:h-[70px]"/>
+          <div className="text-4xl sm:text-5xl text-orange-500 whitespace-nowrap font-Nunito-ExtraBold">
+            <Typewriter
+              options={{
+                strings: ['Big Brain', 'Big Fun', 'Think Fast', 'Game On!'],
+                autoStart: true,
+                loop: true,
+                pauseFor: 5000,
+                delay: 100,
+                deleteSpeed: 75,
+              }}
+            />
+          </div>
+        </div>
+
+        {/* Game Pin */}
+        <div className='px-4 pt-4 pb-5 rounded-xl bg-white'>
+          <div className="mb-3">
+            <input
+              type="text"
+              title="Please enter numbers only"
+              id="sessionIdInput"
+              name="sessionIdInput"
+              aria-label="Game ID"
+              placeholder="Enter Game PIN"
+              autoComplete="off"
+              required
+              value={sessionIdInput}
+              onChange={(e) => {
+                const value = e.target.value.trimStart();
+                setSessionIdInput(value);
+                if (value === '') {
+                  setErrorMessage('');
+                } else if (!/^[0-9]+$/.test(value)) {
+                  setErrorMessage('Game PIN must be numbers only');
+                } else {
+                  setErrorMessage('');
+                }
+              }}
+              onFocus={() => setErrorMessage('')}
+              className="placeholder:font-Nunito-Bold placeholder:text-gray-400 p-3 text-lg border-2 border-gray-200 rounded-md w-[290px] text-center focus:border-orange-400 focus:ring-orange-300 focus:outline-none focus:ring focus:ring-opacity-400 text-md font-Nunito-Bold"
+            />
+          </div>
+
+          {/* Nickname */}
+          <div className="mb-3">
+            <input
+              type="text"
+              id="nickName"
+              name="nickName"
+              aria-label="Player Name"
+              placeholder="Enter Nickname"
+              autoComplete="off"
+              required
+              value={nickName}
+              onChange={(e) => {
+                setNickName(e.target.value);
+              }}
+              onFocus={() => {
+                setErrorMessage('');
+                setIsNickFocused(true);
+              }}
+              onBlur={() => setIsNickFocused(false)}
+              className="placeholder:font-Nunito-Bold placeholder:text-gray-400 p-3 text-lg border-2 border-gray-200 rounded-md w-[290px] text-center focus:border-orange-400 focus:ring-orange-300 focus:outline-none focus:ring focus:ring-opacity-400 font-Nunito-Bold"
+            />
+          </div>
+
+          <button
+            type="submit"
+            disabled={loading || !(sessionIdInput && nickName)}
+            aria-label="Join game now"
+            className={`sm:text-xl w-full px-8 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out shadow-[0_4px_0_0_#c2410c]
+              ${ loading || !(sessionIdInput && nickName) ? 'bg-orange-300 cursor-not-allowed text-white' : 'bg-orange-500 text-white hover:bg-orange-400 hover:-translate-y-1'}`}
+          >
+            {loading ? 'Joining...' : 'Join'}
+          </button>
+        </div>
+      </form>
+      {/^[0-9]+$/.test(sessionIdInput) && !nickName && !isNickFocused && (<div className="fixed bottom-0 left-0 right-0 z-50 bg-green-600 text-white text-lg font-Nunito-Medium flex justify-center items-center py-5 font-semibold">
+        <span>Great now we just need your name!</span>
+      </div>)}
+
+      {/* Error Popup */}
+      {errorMessage && (
+        <div className="fixed bottom-0 left-0 right-0 z-50 bg-red-600 text-white text-lg font-Nunito-Medium flex justify-center items-center py-5 font-semibold">
+          <span>{errorMessage}</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default GameJoin;
