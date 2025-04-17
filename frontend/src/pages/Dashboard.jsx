ef4b8085ab948587ac815ee2b9c1de625a600f20
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-18 05:13:59 +1000

    Backend and docs update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuthContext } from "../context/useAuthContext";
-import LogoNavBar from "../component/LogoNavBar";
+import LinkLogoNavBar from '../component/LinkLogoNavBar';
 import { RiAddCircleLine } from "react-icons/ri";
 import { orangeButtonClass } from "../component/tailwind";
 import {
@@ -10,8 +10,15 @@ import {
   createGameSession,
 } from "../util/gamesApi";
 import GameDashboardTile from "../component/GameDashboardTile";
+
+import JoinGameButton from '../component/JoinGameButton';
+import { VscThreeBars } from "react-icons/vsc";
+import { TbLogout } from "react-icons/tb";
+import { FaPlay } from "react-icons/fa";
+
 import DeleteConfirmationModal from "../component/DeleteConfirmationModal";
 import SessionStartModal from "../component/SessionStartModal";
+
 
 function Dashboard() {
   /*
@@ -152,31 +159,58 @@ function Dashboard() {
 
   return (
     <div className="min-h-screen overflow-y-auto flex flex-col">
-      {/* Navbar */}
+      {/* Navbar Left Side*/}
       <nav className="flex justify-between items-center px-4 sm:px-8 py-2.5 bg-cyan-200 h-[65px]">
-        <Link
-          to="/home"
-          className="text-orange-500 text-3xl font-bold no-underline"
-        >
-          <LogoNavBar />
-        </Link>
-        <div className="flex gap-3 items-center">
-          <Link
-            to="/quiz/create"
-            className={`${orangeButtonClass} flex items-center gap-2`}
-          >
-            <RiAddCircleLine className="text-2xl" /> Create
-          </Link>
-          <Link to="/auth/logout" className={`${orangeButtonClass} px-5`}>
-            Log out
-          </Link>
+        <LinkLogoNavBar targetPath="/home" />
+        {/* //* NavBar Right side For Small Screen Dropdown */}
+        <div className="dropdown dropdown-bottom dropdown-end sm:hidden">
+          {/* Toggle Button */}
+          <div tabIndex={0} role="button" className={orangeButtonClass}>
+            <VscThreeBars className="text-3xl" />
+          </div>
+
+          {/* Dropdown Content */}
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow-lg bg-orange-500 rounded-box w-52 z-50 text-xl text-white">
+            <li>
+              <Link to="/quiz/create" className="flex items-center gap-[16px]">
+                <RiAddCircleLine /> Create
+              </Link>
+            </li>
+            <li>
+              <Link to="/quiz/join" className="font-semibold flex items-center gap-[20px]">
+                <FaPlay className="text-[16px]"/> Join a game
+              </Link>
+            </li>
+            <li>
+              <Link to="/auth/logout" className="flex items-center gap-3">
+                <TbLogout className="text-2xl"/> Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* //*NavBar Right side For Big Screen Dropdown */}
+        <div className="hidden sm:block">
+          <div className="flex gap-3 items-center">
+            {/* Create */}
+            <Link to="/quiz/create" className={`${orangeButtonClass} flex items-center gap-2`}>
+              <RiAddCircleLine className="text-2xl" /> Create
+            </Link>
+            {/* Play */}
+            <JoinGameButton />
+            {/* Logout */}
+            <Link to="/auth/logout" className={`${orangeButtonClass} px-5 flex items-center gap-2`}>
+              <TbLogout className="text-2xl"/> Logout
+            </Link>
+          </div>
         </div>
       </nav>
 
-      {/* Games Feed */}
+
+      {/* //*Games Feed */}
       <div className="flex-1 flex flex-col justify-start items-center text-center p-8">
         <div className="w-full max-w-6xl">
-          <h1 className="text-5xl font-semibold text-left mb-6 text-orange-500 font-Nunito-Black">
+          <h1 className="text-5xl font-semibold text-left mb-6 text-orange-500 font-Nunito-ExtraBold">
             My Games
           </h1>
 
