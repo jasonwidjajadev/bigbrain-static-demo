59da2932a8e7a6cf0cb957be6bb5b7623ac240f9
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-18 21:30:54 +1000

    added additional text

--- frontend/src/pages/session/InactiveGame.jsx
+++ frontend/src/pages/session/InactiveGame.jsx
@@ -15,10 +15,11 @@ function InactiveGame() {
         <LinkLogoNavBar targetPath={targetPath} />
       </nav>
 
-      <div className="flex-1 flex flex-col justify-center items-center text-center pt-8 pb-20">
+      <div className="flex-1 flex flex-col justify-center items-center text-center pt-0 sm:pt-8 pb-20">
         <Lottie animationData={BrainInactiveQuiz} loop={true} autoplay={true} style={{ height: 400 }} />
 
-        <h1 className="text-3xl sm:text-5xl font-Nunito-ExtraBold mb-9">Oops, this quiz is inactive!</h1>
+        <h1 className="text-3xl sm:text-5xl font-Nunito-ExtraBold mb-9">Oops!</h1>
+        <p className="text-gray-500 mb-3 sm:text-lg pb-3">The Game PIN is wrong <br className='block sm:hidden'/>or this quiz is no longer active!</p>
         <p className="text-black sm:text-lg">Please join another quiz <Link to='/join' className="text-blue-500">here</Link></p>
       </div>
     </div>
