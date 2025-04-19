e38ae98c92482a7c54f50fc5c7f51a2bb0a2c6cf
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-20 06:08:43 +1000

    added rc slider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
+import 'rc-slider/assets/index.css';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
