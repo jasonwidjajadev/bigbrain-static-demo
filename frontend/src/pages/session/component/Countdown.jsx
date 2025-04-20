e56829e87678f980f2e06f266d8b2cda3e28ac7f
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-21 05:25:56 +1000

    worked on fuctionality for player game

--- frontend/src/pages/session/component/Countdown.jsx
+++ frontend/src/pages/session/component/Countdown.jsx
@@ -34,12 +34,17 @@ function Countdown({position, length, question, onComplete}) {
       style={{ backgroundImage: `url(${classroom})` }}
     >
       <div className='flex-1 flex flex-col justify-center items-center pb-20'>
-        <h1 className="text-5xl sm:text-7xl font-Nunito-ExtraBold mb-15 text-black">Question {position}/{length}</h1>
+        {position && length && <h1 className="text-5xl sm:text-7xl font-Nunito-ExtraBold mb-15 text-black">Question {position}/{length}</h1>}
+        {!position && !length && <h1 className="text-5xl sm:text-7xl font-Nunito-ExtraBold mb-15 text-black">Countdown</h1>}
         <div className="text-7xl font-Nunito-Bold h-35 w-35 rounded-full bg-orange-500 flex justify-center items-center text-white shrink-0 mb-10">
           {count}
         </div>
-        <div className="font-Nunito-Bold px-6 py-5 bg-cyan-800 text-2xl border-none rounded-md text-white mb-4">{text}</div>
-        <div className="w-full font-Nunito-ExtraBold p-5 bg-cyan-500 text-xl border-none rounded-md text-white no-underline shadow-[0_4px_0_0_#0e7490] transition-all duration-300 ease-in-out hover:bg-cyan-400 hover:-translate-y-1 break-words">{description}</div>
+        {question && (
+          <>
+            <div className="font-Nunito-Bold px-6 py-5 bg-cyan-800 text-2xl border-none rounded-md text-white mb-4">{text}</div>
+            <div className="w-full font-Nunito-ExtraBold p-5 bg-cyan-500 text-xl border-none rounded-md text-white no-underline shadow-[0_4px_0_0_#0e7490] transition-all duration-300 ease-in-out hover:bg-cyan-400 hover:-translate-y-1 break-words">{description}</div>
+          </>
+        )}
       </div>
     </main>
   )
