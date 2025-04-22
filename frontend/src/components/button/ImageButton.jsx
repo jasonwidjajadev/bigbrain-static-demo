7dbf97eb2386f2164f893560b7982c24b6c25822
MarkBastoulis <mbastoulis@gmail.com>
2025-04-22 13:26:51 +1000

    Updated buttons to be of type button

--- frontend/src/components/button/ImageButton.jsx
+++ frontend/src/components/button/ImageButton.jsx
@@ -4,10 +4,11 @@ import { blueButtonClassSmall } from "@/components/ui/tailwind";
 const ImageButton = ({ onClick }) => {
   return (
     <button
+      type="button"
       onClick={onClick}
       className={`flex justify-center items-center w-[60%] md:w-[30%] lg:w-[30%] gap-2 ${blueButtonClassSmall} min-w-[100px]`}
     >
-      <LuImage size={24}  className="shrink-0"/>
+      <LuImage size={24} className="shrink-0" />
       <span className="text-sm font-small">Image</span>
     </button>
   );
