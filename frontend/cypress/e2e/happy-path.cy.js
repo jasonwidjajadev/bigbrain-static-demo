d0b66afbd807bdde903a2b12da9500bf06e29e94
Jason Widjaja <z5494973@ad.unsw.edu.au>
2025-04-22 16:59:58 +1000

    added cypress

---
+++ frontend/cypress/e2e/happy-path.cy.js
@@ -0,0 +1,33 @@
+// https://gist.github.com/in2Unknown/1f4b682f5a244b3537dea24c8d4b53ed
+
+describe('template spec', () => {
+  it('passes', () => {
+    window.cy.visit('https://example.cypress.io')
+  })
+})
+
+// Write a test for the "happy path" of an admin that is described as:
+
+
+// 1. Registers successfully
+
+
+// 2. Creates a new game successfully
+
+
+// 3. (Not required) Updates the thumbnail and name of the game successfully (yes, it will have no questions)
+
+
+// 4. Starts a game successfully
+
+
+// 5. Ends a game successfully (yes, no one will have played it)
+
+
+// 6. Loads the results page successfully
+
+
+// 7. Logs out of the application successfully
+
+
+// 8. Logs back into the application successfully
