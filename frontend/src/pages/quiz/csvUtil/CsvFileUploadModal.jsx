babe0235b7e3935bbb3e80c2622c05ea2a296e31
MarkBastoulis <mbastoulis@gmail.com>
2025-04-22 22:11:27 +1000

    Changed csv upload to create a whole quiz

--- frontend/src/pages/quiz/csvUtil/CsvFileUploadModal.jsx
+++ frontend/src/pages/quiz/csvUtil/CsvFileUploadModal.jsx
@@ -37,7 +37,7 @@ function CsvFileUploadModal({ isOpen, onClose, onFileUpload }) {
   };
 
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 px-6 sm:px-0">
       <div className="bg-white rounded-lg p-6 w-full max-w-md">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-2xl font-semibold text-gray-800">
